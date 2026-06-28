313fb43eb27838370d4a5c9a5ae8cf903c2df354
SHIVA FULARE <fulareshiva894@gmail.com>
2026-06-28 01:48:20 +0000

    container config done

---
+++ 01_basics/test.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "shiva@1234"
+var accountpassword ="11234434"
+let accountState;
+
+/*
+Prefer not to use var 
+because of the issue of bolck scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountpassword,accountState])
