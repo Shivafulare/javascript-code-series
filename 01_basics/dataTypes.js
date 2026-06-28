e89211ce97eae667267b1cca8c615ababa1d9f4b
SHIVA FULARE <fulareshiva894@gmail.com>
2026-06-28 02:16:53 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,15 @@
+let name = "Hitesh"
+let age = 18
+
+// number => 2 to power 53 
+// bigint
+// string=> ""
+// boolean => true/false
+// null => standalone value 
+// undefined=> 
+// symbol => unique 
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof null);
