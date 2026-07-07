0ef2e5824bba70fc78adcada28886cf761be3ba1
SHIVA FULARE <fulareshiva894@gmail.com>
2026-07-07 02:31:22 +0000

    learn about string

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -1,5 +1,5 @@
-let name = "Hitesh"
-let age = 18
+// let name = "Hitesh"
+// let age = 18
 
 // number => 2 to power 53 
 // bigint
@@ -11,5 +11,35 @@ let age = 18
 
 // object
 
-console.log(typeof "hitesh");
-console.log(typeof null);
+// console.log(typeof "hitesh");
+// console.log(typeof null);
+
+const name = " America"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my rapo count is ${repoCount}`)
+
+const gamename= new String('pop-up-down')
+console.log(gamename[3]);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf(2));
+
+const newString= gamename.substring(0,4)
+console.log(newString);
+
+const anotherString= gamename.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "  america   "
+console.log(newStringOne.trim());
+
+const url = "https://america.com/hitesh%20ramanand"
+
+console.log(url.replace('%20', "--"))
+
+console.log(url.includes('america'));
+
+console.log(gamename.split("-"));
